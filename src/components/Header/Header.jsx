b89ee0e65df6b2466d8fb5b7cb1852b2e305f019
Giovani Aguirre <me@its-g.io>
2020-02-07 16:12:02 -0600

    Added light navigation between add item and dashboard

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <header>
+        <h1><Link to="/">Gift Dumps</Link></h1>
+        <ul>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/addProduct">Add Item</Link></li>
+        </ul>
+      </header>
     )
   }
 }
